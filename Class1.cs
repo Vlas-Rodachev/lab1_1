89754cd466ed5eef78ffdae50e184813bcdacde4
влас родачев <roda4evvlas@mail.ru>
2025-04-07 22:23:05 +0500

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_1
+{
+    public class CharInputValidator
+    {
+        public static char GetValidCharInput()
+        {
+            char result;
+            while (true)
+            {
+                Console.Write("Введите символ: ");
+                string input = Console.ReadLine();
+
+                if (char.TryParse(input, out result))
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Введенное значение не является одиночным символом. Попробуйте снова.");
+                }
+            }
+        }
+    }
+
+    public class IntInputValidator
+    {
+        public static int GetValidIntInput()
+        {
+            int result;
+            while (true)
+            {
+                Console.Write("Введите значение: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out result))
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Неверное значение. Попробуйте снова.");
+                }
+            }
+        }
+    }
+
+    public class TwoChars
+    {
+        protected char firstChar;
+        protected char secondChar;
+
+        // Конструктор по умолчанию
+        public TwoChars()
+        {
+            firstChar = 'A';
+            secondChar = 'B';
+        }
+
+        // Конструктор с параметрами
+        public TwoChars(char first, char second)
+        {
+            firstChar = first;
+            secondChar = second;
+        }
+
+        // Конструктор копирования
+        public TwoChars(TwoChars source)
+        {
+            firstChar = source.firstChar;
+            secondChar = source.secondChar;
+        }
+
+        ~TwoChars()
+        {
+            Console.WriteLine($"TwoChars: деструктор (уничтожен объект с символами '{firstChar}', '{secondChar}')");
+        }
+
+        // Метод для создания строки из полей
+        public string CreateStringFromFields()
+        {
+            return $"{firstChar}{secondChar}";
+        }
+
+        // Перегрузка ToString()
+        public override string ToString()
+        {
+            return $"TwoChars: First='{firstChar}', Second='{secondChar}'";
+        }
+    }
+
+    public class EnhancedTwoChars : TwoChars
+    {
+        private int weight;
+
+        // Конструктор по умолчанию
+        public EnhancedTwoChars() : base()
+        {
+            weight = 0;
+        }
+
+        // Конструктор с параметрами
+        public EnhancedTwoChars(char first, char second) : base(first, second)
+        {
+            weight = first + second;
+        }
+
+        ~EnhancedTwoChars()
+        {
+            Console.WriteLine($"EnhancedTwoChars: деструктор (уничтожен объект с весом {weight})");
+        }
+
+        // Метод 1: Проверка, является ли вес положительным
+        public bool IsWeightPositive()
+        {
+            return weight > 0;
+        }
+
+        // Метод 2: Изменение веса на указанное значение
+        public void IncreaseWeight(int amount)
+        {
+            weight += amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Weight='{weight}'";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using lab1_1;
+
+TwoChars fu1()
+{
+    while (true)
+    {
+        Console.WriteLine("Введите номер задания для основного класса");
+        Console.WriteLine("1 - Ввести символы вручную");
+        Console.WriteLine("2 - Оставить базовое значение");
+        int number_problem1 = IntInputValidator.GetValidIntInput();
+        if (number_problem1 == 1)
+        {
+            TwoChars twoChars = new TwoChars(CharInputValidator.GetValidCharInput(), CharInputValidator.GetValidCharInput());
+            Console.WriteLine(twoChars.ToString());
+            Console.WriteLine();
+            return twoChars;
+        }
+        else if (number_problem1 == 2)
+        {
+            TwoChars twoChars = new TwoChars();
+            Console.WriteLine(twoChars.ToString());
+            Console.WriteLine();
+            return twoChars;
+        }
+    }
+}
+
+EnhancedTwoChars fu2()
+{
+    while (true)
+    {
+        Console.WriteLine("Введите номер задания для дочернего класса");
+        Console.WriteLine("1 - Ввести символы вручную");
+        Console.WriteLine("2 - Оставить базовое значение");
+        int number_problem1 = IntInputValidator.GetValidIntInput();
+        if (number_problem1 == 1)
+        {
+            EnhancedTwoChars twoChars = new EnhancedTwoChars(CharInputValidator.GetValidCharInput(), CharInputValidator.GetValidCharInput());
+            Console.WriteLine(twoChars.ToString());
+            Console.WriteLine();
+            return twoChars;
+        }
+        else if (number_problem1 == 2)
+        {
+            EnhancedTwoChars twoChars = new EnhancedTwoChars();
+            Console.WriteLine(twoChars.ToString());
+            Console.WriteLine();
+            return twoChars;
+        }
+    }
+}
+
+TwoChars twoChars = fu1();
+EnhancedTwoChars twoCharsDother = fu2();
+bool tr1 = true;
+while (tr1)
+{
+    Console.WriteLine("Введите номер задания");
+    Console.WriteLine("1 - Скопировать в новый класс");
+    Console.WriteLine("2 - Сделать строку из символов");
+    Console.WriteLine("3 - Проверить положительный ли вес класса");
+    Console.WriteLine("4 - Изменить вес класса");
+    Console.WriteLine("5 - Выход");
+    int number_problem = IntInputValidator.GetValidIntInput();
+    if (number_problem == 1)
+    {
+        TwoChars twoChars1 = new TwoChars(twoChars);
+        Console.WriteLine(twoChars1.ToString());
+        Console.WriteLine();
+    }
+    else if (number_problem == 2)
+    {
+        Console.WriteLine(twoChars.CreateStringFromFields());
+        Console.WriteLine();
+    }
+    else if (number_problem == 3)
+    {
+        Console.WriteLine(twoCharsDother.IsWeightPositive());
+        Console.WriteLine();
+    }
+    else if (number_problem == 4)
+    {
+        twoCharsDother.IncreaseWeight(IntInputValidator.GetValidIntInput());
+        Console.WriteLine(twoCharsDother.ToString());
+        Console.WriteLine();
+    }
+    else if (number_problem == 5)
+    {
+        tr1 = false;
+    }
+}
